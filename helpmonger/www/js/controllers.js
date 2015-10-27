008dae2976f76d214751e428cf7b6b1b664c9346
Omar Ansari <ansario@email.sc.edu>
2015-10-27 13:51:32 -0400

    added rudimentary login, username/password is admin/admin

--- helpmonger/www/js/controllers.js
+++ helpmonger/www/js/controllers.js
@@ -2,12 +2,18 @@ angular.module('starter.controllers', [])
 
 .controller('DashCtrl', function($scope) {})
 
-.controller('LoginCtrl', function($scope, $state) {
-
-    $scope.LogIn = function(user) {
-      $state.go('tab.dash'); //Need to redirect to main screen.
-    };
-
+.controller('LoginCtrl', function($scope, LoginService, $ionicPopup, $state) {
+    $scope.data = {}
+    $scope.login = function() {
+       LoginService.loginUser($scope.data.username, $scope.data.password).success(function(data) {
+           $state.go('tab.dash');
+       }).error(function(data) {
+           var alertPopup = $ionicPopup.alert({
+               title: 'Login failed!',
+               template: 'Your username/password combination is incorrect.'
+           });
+       });
+   }
 })
 
 .controller('RegisterCtrl', function($scope, $state) {
